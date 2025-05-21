6a8b68767874a791f1fe3020b8426d7eb7f0c28c
Minami Takayama <m.inam.ina03037@gmail.com>
2025-05-21 18:50:28 +0900

    can get input value

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,44 @@
+import React, { useState } from "react";
 import './App.css'
 
 function App() {
+  const [inputValue, setInputValue] =useState("");
+  // 型指定あり＜＞
+  const [todos, setTodos] = useState<Todo[]>([]);
+
+  type Todo = {
+    inputValue: string;
+    id: number;
+    checked: boolean;
+  }
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    console.log(e.target.value);
+    setInputValue(e.target.value);
+  }
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) =>{
+    //ボタンを押したときにリロードしないようにする
+    e.preventDefault();
+    const newTodo: Todo ={
+      inputValue: inputValue,
+      id: todos.length,
+      checked: false,
+    };
+
+    setTodos([newTodo, ...todos]);
+    setInputValue("");
+  }
+
+
 
   return (
     <>
     <div className="App">
       <div>
         <h2>ToDo List with Typescript</h2>
-        <form onSubmit ={() =>{}}>
-          <input type="text" onChange={()=>{}}className="inputText"/>
+        <form onSubmit ={(e) =>{handleSubmit(e)}}>
+          <input type="text" onChange={(e)=>{handleChange(e)}}className="inputText"/>
           <input type="submit" value="add" className="submitButton"/>
         </form>
       </div>
